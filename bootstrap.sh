8f78a9a54b4a4e819ddf89c4d4d0233641705d62
Spectrewolf2 <160576560+Spectrewolf2@users.noreply.github.com>
2024-12-09 16:09:01 -0500

    bootstrap command that installs all dependencies

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install git -y
+sudo apt install docker-compose -y
+sudo apt install ansible -y
+sudo snap install terraform --classic
+sudo snap install kubectl --classic
+sudo snap install aws-cli --classic
